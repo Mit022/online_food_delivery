8599654b97d270f869951b795f4c875342df1967
Mit022 <121728029+Mit022@users.noreply.github.com>
2023-10-20 09:58:21 +0530

    Add files via upload

---
+++ OnlineFoodDelivery-Application-main/src/main/java/com/onlinefooddeivery/service/CartService.java
@@ -0,0 +1,21 @@
+package com.onlinefooddeivery.service;
+
+import java.util.List;
+
+import com.onlinefooddeivery.model.Cart;
+import com.onlinefooddeivery.model.Customer;
+
+
+
+public interface CartService {
+
+	Cart addCart(Cart cart,long productId,long customerId);
+	List<Cart> getAllCarts();
+	Cart getCartById(long cartId);
+	Cart updateCart(Cart cart, long cartId);
+	void deleteCart(long cartId);
+	void deleteCartByCustomer(Customer c);
+	
+	
+
+}
